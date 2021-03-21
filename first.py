eff7f0ef5679695ad5da21d545707fec27274b97
xortiz <xavier.ortiz.ch@gmail.com>
2021-03-21 09:58:15 -0400

    some cleanup and light refactoring

--- first.py
+++ first.py
@@ -8,6 +8,7 @@ Rens = ['10000030','60004588'] # Heimatar
 Hek = ['10000042','60005686'] # Metropolis
 
 market_region_hub = [Jita,Amarr,Dodixie,Rens,Hek]
+# market_region_hub = [Rens]
 
 http=urllib3.PoolManager()
 url_base = 'https://esi.evetech.net/latest/markets/'
@@ -16,7 +17,7 @@ url_end = '/orders/?datasource=tranquility&order_type=all&page='
 def market_p1_data(region_hubs):
   payload = {}
   for region in region_hubs:
-    url = url_base + region[0] + url_end + '1'
+    url = url_creator(region[0], '1')
     page_one = http.request('GET', url)
     region.append(page_one.headers['x-pages'])
     payload[region[0]] = json.loads(page_one.data)
@@ -25,26 +26,19 @@ def market_p1_data(region_hubs):
 def market_data_fetch(region_hubs, raw_data):
   for region in region_hubs:
     for page in range(2, int(region[2]) + 1):
-      url = url_base + region[0] + url_end + str(page)
+      url = url_creator(region[0], str(page))
       answer = http.request('GET', url)
       new_data = json.loads(answer.data)
-      """
-      print(type(new_data))
-      print("paap")
-      print(type(raw_data[region[0]]))
-      print("poop")
-      print(type(raw_data))
-      print("dead")
-      """
       raw_data[region[0]] = raw_data[region[0]] + new_data
-      #print(type(raw_data[region[0]]))
-      #merged_data = old_data.update(new_data)
-      #raw_data[region[0]][0] = merged_data
   return raw_data
 
+def url_creator(region_id, page):
+  return url_base + region_id + url_end + page
+  
 raw_data = market_p1_data(market_region_hub)
 raw_data = market_data_fetch(market_region_hub, raw_data)
 
 print(raw_data.keys())
-print(market_region_hub)
+# print(market_region_hub)
 print(str(len(raw_data[Jita[0]])) + ", " + str(len(raw_data[Amarr[0]])) + ", " + str(len(raw_data[Dodixie[0]])) + ", " + str(len(raw_data[Rens[0]])) + ", " + str(len(raw_data[Hek[0]])))
+print(str(len(raw_data[Rens[0]])))
