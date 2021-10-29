2b81ea5dbafa97fa9810db71b434b7ae837820bd
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2021-10-29 17:41:14 -0400

    Reworked  as a way to remember the workflows, serially. After, reworked \request-futures` for pulling `page 1` of the market order data. as I've become more familiar with this library.

--- async.py
+++ async.py
@@ -1,91 +1,97 @@
-import aiohttp
-import asyncio
-import json
+import json, os
+from requests.exceptions import HTTPError, RequestException
+from requests_futures.sessions import FuturesSession
+from concurrent.futures import as_completed
+
+session = FuturesSession(max_workers=200)
 
 # High sec market hubs only, [RegionID, StationID]
 
-jita = ['10000002','60003760'] # The Forge
-amarr = ['10000043','60008494'] # Domain
-dodixie = ['10000032','60011866'] # Sinq Liason
-rens = ['10000030','60004588'] # Heimatar
-hek = ['10000042','60005686'] # Metropolis
+Jita = ['10000002','60003760'] # The Forge
+Amarr = ['10000043','60008494'] # Domain
+Dodixie = ['10000032','60011866'] # Sinq Liason
+Rens = ['10000030','60004588'] # Heimatar
+Hek = ['10000042','60005686'] # Metropolis
 
 url_base = 'https://esi.evetech.net/latest/markets/'
 url_end = '/orders/?datasource=tranquility&order_type=all&page='
 
-market_region_hub = [jita,amarr,dodixie,rens,hek]
-#market_region_hub = [rens]
-async def test():
-  async with aiohttp.ClientSession() as session:
-    async with session.get(url_creator(market_region_hub[0][0], '1')) as response:
-      data = await response.text()
-      pages = response.headers['x-pages']
-#      print(data)
-#      print(pages)
-      return data, pages
+region_hubs = [Jita,Amarr,Dodixie,Rens,Hek]
 
-async def market_p1_data(region_hubs):
-  payload = {}
-  async with aiohttp.ClientSession() as session:
-    for region in region_hubs:
-      url = url_creator(region[0], '1') 
-      async with session.get(url) as response:
-        page_one = response.headers['x-pages']
-        region.append(page_one)
-        data = await response.text()
-        payload[region[0]] = json.loads(data)
-  return payload
+def create_page1_market_region_futures(region_hubs):
+  raw_data = {}
+  futures = []
+  for region in region_hubs:
+    url = create_url(region[0], '1')
+    future =  session.get(url)
+    future.region_hub = region
+    futures.append(future)
+  return futures
 
-def url_creator(region_id, page):
+def get_page1_market_region_results(futures, redo_region_hub, error_write):
+  for response in as_completed(futures):
+    result = response.result()
+    try:
+      result.raise_for_status()
+      error_limit_remaining = result.headers['x-esi-error-limit-remain']
+      if error_limit_remaining != "100":
+          error_limit_time_to_reset = result.headers['x-esi-error-limit-reset']
+          error_write.write('INFORMATIONAL: Though no error, for {} the Error Limit Remaning: {} Limit-Rest {} \n\n'.format(result.url, error_limit_remaining, error_limit_time_to_reset))
+    except HTTPError:
+      error_write.write('Received status code {} from {} With headers:\n{}\n'.format(result.status_code, result.url, str(result.headers)))
+      if 'x-esi-error-limit-remain' in result.headers:
+          error_limit_remaining = result.headers['x-esi-error-limit-remain']
+          error_limit_time_to_reset = result.headers['x-esi-error-limit-reset']
+          error_write.write('Error Limit Remaing: {} Limit-Rest {} \n'.format(error_limit_remaining, error_limit_time_to_reset))
+      error_write.write("\n")
+      redo_region_hub.append(response.region_hub)
+      continue
+    except RequestException as e: 
+      error_write.write("other error is " + e + "\n")
+      continue
+    orders = json.loads(result.text)
+    total_pages = result.headers["x-pages"]
+    raw_data[response.region_hub[0]] = {
+      'orders': orders,
+      'pages': total_pages
+      }
+  return raw_data, redo_region_hub
+
+def get_page1_market_region_data(region_hubs, raw_data, error_write):
+  redo_region_hub = []
+  futures = create_page1_market_region_futures(region_hubs)
+  raw_data, redo_region_hub = get_page1_market_region_results(futures, redo_region_hub, error_write)
+  if len(redo_region_hub) != 0:
+    # recursion - need to think if futures need to be re-created, if futures that need to be redone can 
+    # be separated... maybe can be optimized
+    raw_data = get_page1_market_region_data(redo_region_hub, raw_data, error_write)
+  return raw_data
+    
+def create_market_order_futures(region_hubs):
+  futures = {}
+  return futures
+
+def get_market_order_results():
+  results = 1
+  return results
+
+def get_market_orders():
+  market_orders = {}
+  return market_orders
+
+def create_url(region_id, page):
   return url_base + region_id + url_end + page
 
-async def market_data_fetch(region_hubs, raw_data):
-  async with aiohttp.ClientSession() as session:
-    for region in region_hubs:
-      for page in range(2, int(region[2]) + 1):
-        url = url_creator(region[0], str(page))
-        async with session.get(url) as response:
-          answer = await response.text()
-          new_data = json.loads(answer)
-          raw_data[region[0]] = raw_data[region[0]] + new_data
-  return raw_data
-
-
-loop = asyncio.get_event_loop()
-raw_data = loop.run_until_complete(market_p1_data(market_region_hub))
-raw_data = loop.run_until_complete(market_data_fetch(market_region_hub, raw_data))
-
-print(raw_data.keys())
-print(market_region_hub)
-print(str(len(raw_data[jita[0]])) + ", " + str(len(raw_data[amarr[0]])) + ", " + str(len(raw_data[dodixie[0]])) + ", " + str(len(raw_data[rens[0]])) + ", " + str(len(raw_data[hek[0]])))
-#print(str(len(raw_data[rens[0]])))
-
-"""
-http=urllib3.PoolManager()
-
-def market_p1_data(region_hubs):
-  payload = {}
-  for region in region_hubs:
-    url = url_creator(region[0], '1')
-    page_one = http.request('GET', url)
-    region.append(page_one.headers['x-pages'])
-    payload[region[0]] = json.loads(page_one.data)
-  return payload
-
 def market_data_fetch(region_hubs, raw_data):
-  for region in region_hubs:
-    for page in range(2, int(region[2]) + 1):
-      url = url_creator(region[0], str(page))
-      answer = http.request('GET', url)
-      new_data = json.loads(answer.data)
-      raw_data[region[0]] = raw_data[region[0]] + new_data
   return raw_data
+ 
 
-raw_data = market_p1_data(market_region_hub)
-raw_data = market_data_fetch(market_region_hub, raw_data)
-
-print(raw_data.keys())
-# print(market_region_hub)
-print(str(len(raw_data[jita[0]])) + ", " + str(len(raw_data[amarr[0]])) + ", " + str(len(raw_data[dodixie[0]])) + ", " + str(len(raw_data[rens[0]])) + ", " + str(len(raw_data[hek[0]])))
-print(str(len(raw_data[rens[0]])))
-"""
+raw_data = {}
+if not os.path.isdir('./data/error'):
+  os.makedirs('./data/error') 
+  print("error directory created")
+error_write = open('./data/error/order.txt','w+')
+#raw_data, redo_region_hub = get_page1_market_region_results(futures, region_hubs, redo_region_hub, error_write)
+get_page1_market_region_data(region_hubs, raw_data, error_write)
+print(str(len(raw_data[Jita[0]]['orders'])) + ", " + str(len(raw_data[Amarr[0]]['orders'])) + ", " + str(len(raw_data[Dodixie[0]]['orders'])) + ", " + str(len(raw_data[Rens[0]]['orders'])) + ", " + str(len(raw_data[Hek[0]]['orders'])))
+print(str((raw_data[Jita[0]]['pages'])) + ", " + str((raw_data[Amarr[0]]['pages'])) + ", " + str((raw_data[Dodixie[0]]['pages'])) + ", " + str((raw_data[Rens[0]]['pages'])) + ", " + str((raw_data[Hek[0]]['pages'])))
--- sync.py
+++ sync.py
@@ -7,38 +7,42 @@ Dodixie = ['10000032','60011866'] # Sinq Liason
 Rens = ['10000030','60004588'] # Heimatar
 Hek = ['10000042','60005686'] # Metropolis
 
-market_region_hub = [Jita,Amarr,Dodixie,Rens,Hek]
-# market_region_hub = [Rens]
+region_hub = [Jita,Amarr,Dodixie,Rens,Hek]
 
 http=urllib3.PoolManager()
 url_base = 'https://esi.evetech.net/latest/markets/'
 url_end = '/orders/?datasource=tranquility&order_type=all&page='
 
-def market_p1_data(region_hubs):
-  payload = {}
+def market_page1_data(region_hubs):
+  raw_data = {}
   for region in region_hubs:
     url = url_creator(region[0], '1')
+
     page_one = http.request('GET', url)
-    region.append(page_one.headers['x-pages'])
-    payload[region[0]] = json.loads(page_one.data)
-  return payload
+    total_pages = page_one.headers['x-pages']
+
+    orders = json.loads(page_one.data)
+
+    raw_data[region[0]] = {
+      'orders': orders,
+      'pages': total_pages
+      }
+  return raw_data
 
 def market_data_fetch(region_hubs, raw_data):
   for region in region_hubs:
-    for page in range(2, int(region[2]) + 1):
+    for page in range(2, int(raw_data[region[0]]['pages']) + 1):
       url = url_creator(region[0], str(page))
       answer = http.request('GET', url)
-      new_data = json.loads(answer.data)
-      raw_data[region[0]] = raw_data[region[0]] + new_data
+      orders = json.loads(answer.data)
+      raw_data[region[0]]['orders'] = raw_data[region[0]]['orders'] + (orders)
   return raw_data
 
 def url_creator(region_id, page):
   return url_base + region_id + url_end + page
-  
-raw_data = market_p1_data(market_region_hub)
-raw_data = market_data_fetch(market_region_hub, raw_data)
-
-print(raw_data.keys())
-# print(market_region_hub)
-print(str(len(raw_data[Jita[0]])) + ", " + str(len(raw_data[Amarr[0]])) + ", " + str(len(raw_data[Dodixie[0]])) + ", " + str(len(raw_data[Rens[0]])) + ", " + str(len(raw_data[Hek[0]])))
-print(str(len(raw_data[Rens[0]])))
+'''  
+page1_raw_data = market_page1_data(region_hub)
+raw_data = market_data_fetch(region_hub, page1_raw_data)
+print(str(len(raw_data[Jita[0]]['orders'])) + ", " + str(len(raw_data[Amarr[0]]['orders'])) + ", " + str(len(raw_data[Dodixie[0]]['orders'])) + ", " + str(len(raw_data[Rens[0]]['orders'])) + ", " + str(len(raw_data[Hek[0]]['orders'])))
+print(str((raw_data[Jita[0]]['pages'])) + ", " + str((raw_data[Amarr[0]]['pages'])) + ", " + str((raw_data[Dodixie[0]]['pages'])) + ", " + str((raw_data[Rens[0]]['pages'])) + ", " + str((raw_data[Hek[0]]['pages'])))
+'''
