fbadd4bbf8500f09b343e6e62585fb3fbe904e49
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2025-02-11 11:32:08 -0500

    Issue #6: Remove config information from main script
    - Separating the config from script in order to avoid putting in toon information in scripts
    - config.example.py was given as an example config
    - Minor cosmetic cleanups
    - Rename a function to reflect what it's doing better

---
+++ config.example.py
@@ -0,0 +1,5 @@
+# When config is ready, rename to config.py to allow the script to use information
+region_hubs = {
+        "RegionName1": ["RegionId1", "StationId1", "ToonId1"], # Jita should be this first entry
+        "RegionName2": ["RegionId2", "StationId2", "ToonId2"],
+        }
