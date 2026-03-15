38a90c966c531b40e6ef1055bb980eec28f7a389
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-03-15 12:51:59 -0400

    Issue #57: Cleanup deserialize.py

--- processing/deserialize.py
+++ processing/deserialize.py
@@ -28,8 +28,7 @@ def deserialize_order_item_p1(
     region: str, func: Callable[[str, int], str]
 ) -> tuple[list[Order], int]:
     deserialized_results = []
-    # `p1_results` are the raw results of a first page of a request
-    #   `redo_urls` is a list of URLs that were not able to be loaded, and require to be
+    # `redo_urls` is a list of URLs that were not able to be loaded, and require to be
     #   requesting again
     fr = cl.futures_results(cl.create_futures([func(region, 1)]))
     cl.pause_futures(
@@ -42,10 +41,11 @@ def deserialize_order_item_p1(
             fr.error_timer,
             f"Sleep p1 order fetch due to error timer being {fr.error_timer} seconds",
         )
+    # `p1_deserialized_results` are the deserialized results of a first page of a
+    #   request
     p1_deserialized_result: Order = json.loads(fr.results[0].text)
     total_pages = int(fr.results[0].headers["x-pages"])
     deserialized_results += p1_deserialized_result
-    # print(deserialized_results)
     return deserialized_results, total_pages
 
 
@@ -61,7 +61,7 @@ def deserialize_order_items_p2_onwards(
         url = func(region, page)
         if (page - 2) % chunk_length == 0:
             urls.append([])
-        # urls is a list of (at most 100 url) lists.
+        # urls is a list of url lists of length `chunk_length`.
         urls[(page - 2) // chunk_length].append(url)
     for chunk_urls in urls:
         pages_futures = cl.create_futures(chunk_urls)
@@ -97,7 +97,10 @@ def deserialize_order_items(
         deserialized_results, total_pages = deserialize_order_item_p1(region, func)
     if len(redo_urls) == 0:
         deserialized_results, redo_urls = deserialize_order_items_p2_onwards(
-            region, total_pages, deserialized_results, func
+            region,
+            total_pages,
+            deserialized_results,
+            func,
         )
     return deserialized_results, redo_urls
 
@@ -128,7 +131,7 @@ def get_source_data(region: str, global_orders: GlobalOrders) -> None:
         active_order_names=[],
         all_order_history=[],
     )
-    # Fetches all orders in a region. regional_orders[region]["allOrdersData"] looks
+    # Fetches all orders in a region. regional_orders[region].all_orders_data looks
     #  like:
     #
     # [{'duration': 90, 'is_buy_order': False, 'issued': '2025-01-28T20:37:14Z',
@@ -139,10 +142,12 @@ def get_source_data(region: str, global_orders: GlobalOrders) -> None:
     #   https://developers.eveonline.com/api-explorer#/operations/GetMarketsRegionIdOrders
     region_name = region_hubs[region][0]
     global_orders[region].all_orders_data = deserialize_order_items(
-        region_name, [], u.create_all_order_url
+        region_name,
+        [],
+        u.create_all_order_url,
     )[0]
     # Fetches all Active order item IDs in a region extracted from
-    #   regional_orders[region]["allOrdersData"] fetched from line above,
+    #   regional_orders[region].all_orders_data fetched from line above,
     #   region_item_ids looks like:
     #
     #   [31316, 31318, 27065, 31320, 31322, ...]
@@ -150,7 +155,7 @@ def get_source_data(region: str, global_orders: GlobalOrders) -> None:
 
     # List of dictionaries containing category, id, and name data, used to extract name
     #   data for later processing
-    #   regional_orders[region]["active_order_names"] looks like:
+    #   regional_orders[region].active_order_names looks like:
     #
     # [{'category': 'inventory_type', 'id': 54360,
     #   'name': "Women's Azure Abundance Jacket"}, {'category': 'inventory_type',
@@ -159,16 +164,8 @@ def get_source_data(region: str, global_orders: GlobalOrders) -> None:
     clean_regional_order_data_and_names = an.remove_bad_orders(
         global_orders, region, region_item_ids
     )
-    # regional_orders[region]["allOrdersData"] = clean_regional_order_data_and_names[0]
     global_orders[region].all_orders_data = clean_regional_order_data_and_names[0]
-    # regional_orders[region]["active_order_names"]
-    #   = clean_regional_order_data_and_names[1]
     global_orders[region].active_order_names = clean_regional_order_data_and_names[1]
     region_item_ids = clean_regional_order_data_and_names[2]
     if INCLUDE_HISTORY:
         c.get_source_history_data(region, global_orders, region_item_ids)
-
-
-# some functions here have gz, csv, and other stuff. Adding here because it seems that
-#   they support the deserialization to a certain degree. However, this may not be the
-#   case as they might be better served living in `csv.py`.
